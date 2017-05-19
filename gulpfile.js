71cfddc2b3ac700ccd687fee68ca80fde445230f
Tjin Au Yeung <tjin@usabilla.com>
2017-05-19 13:14:02 +0200

    Include assets in gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,24 @@ const browserSync = require('browser-sync');
 const autoprefixer = require('gulp-autoprefixer');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
+const runSequence = require('run-sequence');
 const cssnano = require('gulp-cssnano');
+const del = require('del');
+
+gulp.task('clean', function() {
+  return del('dist');
+});
+
+gulp.task('copy', function() {
+  return gulp.src([
+    'src/assets/**',
+    '!src/modules'
+  ])
+    .pipe(gulp.dest('dist/assets'));
+});
 
 gulp.task('css', function() {
-  gulp.src('src/scss/main.scss')
+  return gulp.src('src/scss/main.scss')
     .pipe(sass({errLogToConsole: true}))
     .pipe(autoprefixer('last 2 version'))
     .pipe(gulp.dest('dist/css'))
@@ -18,7 +32,7 @@ gulp.task('css', function() {
 });
 
 gulp.task('js', function() {
-  gulp.src('src/js/main.js')
+  return gulp.src('src/js/main.js')
     .pipe(gulp.dest('dist/js'))
     .pipe(uglify())
     .pipe(rename({ suffix: '.min' }))
@@ -29,7 +43,7 @@ gulp.task('js', function() {
 gulp.task('browser-sync', function() {
   browserSync.init(null, {
     server: {
-      baseDir: "."
+      baseDir: '.'
     }
   });
 });
@@ -38,8 +52,13 @@ gulp.task('bs-reload', function() {
   browserSync.reload();
 });
 
-gulp.task('default', ['css', 'js', 'browser-sync'], function() {
-  gulp.watch("src/scss/*/*.scss", ['css']);
-  gulp.watch("src/js/*.js", ['js']);
-  gulp.watch("*.html", ['bs-reload']);
+gulp.task('build', function() {
+  return runSequence('clean', 'copy', 'css', 'js');
+});
+
+gulp.task('default', ['clean', 'copy', 'css', 'js', 'browser-sync'], function() {
+  gulp.watch('src/assets/**', ['assets']);
+  gulp.watch('src/scss/*/*.scss', ['css']);
+  gulp.watch('src/js/*.js', ['js']);
+  gulp.watch('*.html', ['bs-reload']);
 });
