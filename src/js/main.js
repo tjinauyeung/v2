2be9caa478908ff299f6b17e91abe31211fb8c80
Tjin Au Yeung <tjin@usabilla.com>
2017-05-19 07:35:06 +0200

    Cleaned up js

---
+++ src/js/main.js
@@ -0,0 +1,24 @@
+(function(window, document) {
+  'use strict';
+
+  const app = {};
+
+  function initThemeSwitch() {
+    const themeSwitch = document.querySelector('.theme-switch');
+    const page = document.querySelector('html');
+    const sfx = document.querySelector('#sfx-switch');
+
+    function handleSwitch(event) {
+      page.classList.toggle('theme-inverted');
+      sfx.currentTime = 0;
+      sfx.play();
+    }
+
+    sfx.volume = 0.5;
+
+    themeSwitch.addEventListener('click', handleSwitch);
+  }
+
+  app.initThemeSwitch();
+
+})(window, document);
