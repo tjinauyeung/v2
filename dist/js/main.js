9f2d0675db587f3d1641ca7112c131f1f4fedcac
Tjin Au Yeung <tjin@usabilla.com>
2017-05-25 00:57:03 +0200

    Build v0.1.3
    - Add typography switch
    - Update theme colors
    - Add Freight font
    - Add new profile picture

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,24 +2,59 @@
   'use strict';
 
   function initThemeSwitch() {
-    const themeSwitch = document.querySelector('#theme-switch');
-    const page = document.querySelector('html');
-    const sfx = document.querySelector('#sfx-switch');
+    var themeSwitch = document.querySelector('#theme-switch'),
+        page = document.querySelector('html'),
+        sfx = document.querySelector('#sfx-switch');
 
-    function handleSwitch(event) {
+    function handleSwitch() {
       page.classList.toggle('theme-inverted');
       sfx.currentTime = 0;
       sfx.play();
     }
 
     sfx.volume = 0.5;
-
     themeSwitch.addEventListener('click', handleSwitch);
+  }
+
+  function initTypographySwitch() {
+    var typographySwitch = document.querySelector('#typography-switch'),
+        page = document.querySelector('html'),
+        activeIndex = 0,
+
+        TYPOGRAPHY_CLASSES = [
+          'sans-serif',
+          'serif',
+        ];
+
+    function handleTypography() {
+      activeIndex = isLast() ? 0 : activeIndex + 1;
+      return setTypography(TYPOGRAPHY_CLASSES[activeIndex]);
+    }
+
+    function setTypography(className) {
+      removeClasses(page, TYPOGRAPHY_CLASSES);
+      setClass(page, className);
+    }
+
+    function isLast() {
+      return activeIndex >= TYPOGRAPHY_CLASSES.length - 1;
+    }
+
+    function removeClasses(el, classes) {
+      return classes.forEach(c => el.classList.remove(c));
+    }
+
+    function setClass(el, className) {
+      return el.classList.add(className);
+    }
+
+    typographySwitch.addEventListener('click', handleTypography);
   }
 
   function bootstrap(event) {
     if (event.target.readyState === 'interactive') {
       initThemeSwitch();
+      initTypographySwitch();
     }
   };
 
